dbf1d63fe4b20361b854059badc5c57664dbec5f
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-08-02 17:29:30 -0700

    Updated new tests for the project Airbnb

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,17 +1,25 @@
+#!/usr/bin/python3
 import unittest
 from datetime import datetime
-from models.base_model import BaseModel
+from base_model import BaseModel
+
 
 class TestBaseModel(unittest.TestCase):
 
     def initial_test(self):
         object = BaseModel()
         self.assertIsInstance(object.id, str)
-    
+
     def test_str_method(self):
         object = BaseModel()
-        expected_str = f"BaseModel(id={object.id})"
+        expected_str = '[{}] ({}) {}'.format(self.__class__.__name__, self.id,
+                                             self.__dict__)
         self.assertNotEqual(str(object), expected_str)
+
+    def test_time_now(self):
+        object = BaseModel()
+        expected_time = datetime.now()
+        self.assertEqual(object.created_at, expected_time)
 
     def test_time_created(self):
         object = BaseModel()
@@ -21,6 +29,7 @@ class TestBaseModel(unittest.TestCase):
         object = BaseModel()
         full_dict = object.to_dict()
         self.assertNotEqual(full_dict, self.__dict__)
+
 
 if __name__ == '__main__':
     unittest.main()
