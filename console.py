58a39f077b0aff4134c91c0868e60468989667cc
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-08-05 15:44:55 -0700

    Updated the program cmd adding comments

--- console.py
+++ console.py
@@ -6,15 +6,19 @@ command interpreter"""
 
 
 class HBNBCommand(cmd.Cmd):
+    """Class of the console"""
     prompt = '(hbnb)'
 
     def do_quit(self, arg):
+        """exit the program"""
         return True
 
     def do_EOF(self, arg):
+        """exit the program"""
         return True
 
     def emptyline(self):
+        """don´t do anything"""
         return False
 
 
