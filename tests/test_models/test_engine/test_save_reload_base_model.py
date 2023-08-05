1fb7c9254f067903e204cbb9cbb6c17ab0459e25
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-08-05 15:18:06 -0700

    Create unittest for FileStorage project Airbnb

---
+++ tests/test_models/test_engine/test_save_reload_base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import unittest
+from datetime import datetime
+import os
+from models.engine.file_storage import FileStorage
+from models import *
+
+
+class TestFileStorage(unittest.TestCase):
+
+    def test_value(self):
+        self.assertTrue(True)
+
+    def test_type(self):
+        obj = FileStorage()
+        self.assertNotEqual(obj, str)
+
+    def test_path(self):
+        obj = FileStorage.__file_path
+        msg = "__file_path is None"
+        self.assertIsNotNone(obj, msg)
+
+    def test_object(self):
+        obj = FileStorage.__objects
+        msg = "__objects = []"
+        self.assertIsInstance(obj, msg)
+
+if __name__ == '__main__':
+    unittest.main()
