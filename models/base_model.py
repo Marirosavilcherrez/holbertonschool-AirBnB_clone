6d9378cc01bed405f4cc68098c27c45694921d6c
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-08-05 10:25:15 -0700

    Resolving problems in to_dict project Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -37,8 +37,8 @@ class BaseModel:
     def to_dict(self):
         """Public instance return a dictionary containig all keys/value
         and return it with __class__"""
-        full_dict = self.__dict__.copy()
-        full_dict["created_at"] = self.created_at.isoformat()
-        full_dict["updated_at"] = self.updated_at.isoformat()
+        full_dict = self.__dict__
+        full_dict["created_at"] = str(self.created_at.isoformat())
+        full_dict["updated_at"] = str(self.updated_at.isoformat())
         full_dict["__class__"] = self.__class__.__name__
         return full_dict
