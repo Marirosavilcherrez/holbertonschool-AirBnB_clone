41d5730ca4d986ad40eacbff214f73c8f6fe3784
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-08-06 10:32:04 -0700

    Adding more classes to models directory project Airbnb

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+"""This is a class State with a public
+class attributes"""
+
+
+class City(BaseModel):
+    """City class"""
+
+    self.id = str()  # it will be the state.id
+    self.name = str()
