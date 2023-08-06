a063e951fdc448d7f9e1a837c4f03e3d211b3ba5
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-08-06 13:54:25 -0700

    Ficing unitest errors project Airbnb

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 import unittest
-from datetime import datetime
 import os
-import json
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-
+import json
 
 class TestFileStorage(unittest.TestCase):
 
@@ -14,13 +12,12 @@ class TestFileStorage(unittest.TestCase):
         obj = FileStorage()
         setattr(obj, '_FileStorage__file_path', "file")
         self.assertEqual(obj._FileStorage__file_path, "file")
-        print("OK")
 
     def test_object(self):
         objec = FileStorage()
         setattr(objec, '_FileStorage__objects', {})
         self.assertEqual(objec._FileStorage__objects, {})
-        print("OK")
+
 
 if __name__ == '__main__':
     unittest.main()
