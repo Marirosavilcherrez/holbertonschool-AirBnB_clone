a1f90e2cb57c2806b1095bd226ac4fddd64ede36
Silvycr <silvia_alinacr@live.com>
2023-08-06 16:00:31 -0700

    Adding new classes

--- console.py
+++ console.py
@@ -5,9 +5,14 @@ import cmd
 import sys
 import json
 from shlex import split
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,7 +21,12 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
     classes = {
         "BaseModel": BaseModel,
-        "User": User 
+        "User": User,
+        "Amenity": Amenity,
+        "City": City,
+        "Place": Place,
+        "Review": Review,
+        "State": State,
     }
 
     def do_quit(self, arg):
