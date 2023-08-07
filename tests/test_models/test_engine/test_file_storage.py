1da6bc0945612f408d2fbc6cddb7a728aaee2ea1
Silvycr <silvia_alinacr@live.com>
2023-08-06 17:05:40 -0700

    File storage test modification

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-
 import unittest
 import os
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 import json
 
-class TestFileStorage(unittest.TestCase):
+class TestFileStorage(unittest.TestCases):
 
     def test_file_path(self):
-        obj = FileStorage()
-        setattr(obj, '_FileStorage__file_path', "file")
-        self.assertEqual(obj._FileStorage__file_path, "file")
-
-    def test_file_objects(self):
-        objec = FileStorage()
-        setattr(objec, '_FileStorage__objects', {})
-        self.assertEqual(objec._FileStorage__objects, {})
+        file_path = fileStorage.__file_path
+        self.assertIsNotNone(file_path, '__file_path is none')
 
 
 if __name__ == '__main__':
